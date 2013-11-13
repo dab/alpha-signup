94766dedf6d3535b78f9b49aadba9f62560ec1be
Dima Belitsky <dmitry.belitsky@gmail.com>
2013-11-13 23:26:45 +0200

    signup widget block refactoring

--- desktop.bundles/signup-widget/signup-widget.bemjson.js
+++ desktop.bundles/signup-widget/signup-widget.bemjson.js
@@ -10,18 +10,15 @@
             block: 'signup-widget',
             content: [
                 {
-                    block: 'slogan',
-                    content: [
-                        {
-                            elem: 'text',
-                            content: '<span>Interested in contributing to the Alpha stage of High Fidelity?</span> Sign up here, and we\'ll email you when we are ready for help.'
-                        },
-                        {
-                            block: 'button',
-                            text: 'Sign up for Alpha',
-                            url: 'http://highfidelity.io/alpha'
-                        }
-                    ]
+                    elem: 'slogan',
+                    content: '<span>Interested in contributing to the Alpha stage of High Fidelity?</span> Sign up here, and we\'ll email you when we are ready for help.',
+                    mix: { block: 'slogan '}
+                },
+                {
+                    block: 'button',
+                    text: 'Sign up for Alpha',
+                    url: '//highfidelity.io/alpha/',
+                    mods: { type: 'link' }
                 }
 
             ]
